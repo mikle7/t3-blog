72ac34fb43e4e45391935c3e17025f773e0968be
Michael Bell <michael.bell@moneyball.com.au>
2022-07-10 13:40:46 +0800

    Basic tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,29 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors:{
+        primary_blue: '#5865F2',
+        primary_red: '#ED4245',
+        primary_green: '#3BA55D',
+        gray: {
+          900: '#202225',
+          800: '#2f3136',
+          700: '#36393f',
+          600: '#4f545c',
+          400: '#d4d7dc',
+          300: '#e3e5e8',
+          200: '#ebedef',
+          100: '#f2f3f5',
+        }
+      }
+    },
   },
   plugins: [],
 };
